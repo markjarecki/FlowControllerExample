1c4814f6aa95e970f95356de96508ce96c9487f3
Mark Jarecki <markjarecki@fluxom.com>
2019-01-31 15:40:27 +0100

    [MODIFY] Dependency group, groups

--- Modules/SecondScene/SecondScene/SecondScene/AppDelegate.swift
+++ Modules/SecondScene/SecondScene/SecondScene/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -46,7 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
