e034cb14ab7292ac00878dbed4fd291583ddc1b2
Mark Jarecki <markjarecki@fluxom.com>
2019-02-03 20:32:03 +0100

    [ADD] HomeSceneToSecondSceneNavigationDelegate navigation controller delegate

---
+++ FlowControllerExample/FlowControllerExample/Scene flows/Home scene/Flows/ToSecondSceneTap/HomeSceneToSecondSceneNavigationDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  HomeSceneToSecondSceneNavigationDelegate.swift
+//  FlowControllerExample
+//
+//  Created by Mark Jarecki on 3/2/19.
+//  Copyright © 2019 Mark Jarecki. All rights reserved.
+//
+
+import UIKit
+
+// Uses built-in implementation
+extension HomeSceneToSecondSceneTapUserInteractor: UINavigationControllerDelegate {}
